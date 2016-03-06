105430eb6ce087b7babcb5352dd77f7c7d627abb
punkrockingit <william.cosulich@gmail.com>
2016-03-06 15:06:09 -0500

    Task 8 Extracting a Interface
    Used “Extracted an interface” refactoring operation on the Cell.java
    file.

---
+++ Monopoly/src/edu/towson/cis/cosc603/project2/monopoly/IOwnable.java
@@ -0,0 +1,57 @@
+package edu.towson.cis.cosc603.project2.monopoly;
+
+public interface IOwnable {
+
+	/**
+	 * Gets the name.
+	 *
+	 * @return the name
+	 */
+	String getName();
+
+	/**
+	 * Gets the owner.
+	 *
+	 * @return the owner
+	 */
+	Player getOwner();
+
+	/**
+	 * Gets the price.
+	 *
+	 * @return the price
+	 */
+	int getPrice();
+
+	/**
+	 * Checks if is available.
+	 *
+	 * @return true, if is available
+	 */
+	boolean isAvailable();
+
+	/**
+	 * Play action.
+	 */
+	void playAction();
+
+	/**
+	 * Sets the available.
+	 *
+	 * @param available the new available
+	 */
+	void setAvailable(boolean available);
+
+	/**
+	 * Sets the owner.
+	 *
+	 * @param owner the new owner
+	 */
+	void setOwner(Player owner);
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	String toString();
+
+}
